0b45c1fd3b58434caf0dcea48e10ed1328e2c604
Bones McCoy <daniele.murroni@gmail.com>
2015-12-13 19:38:28 +0000

    stylegit add -A!

--- one/js/trophiesofman.js
+++ one/js/trophiesofman.js
@@ -39,4 +39,7 @@ $(document).ready(function(){
             }
         }
     });
+
+    $("#mlto").attr("href", "mailto" + ":info@tr" + "ophiesofman.com");
+    $("#mdress").html("info@tr" + "ophiesofman.com");
 });
